a157af3a0eff63f4e1e7bcb9a2e67ced80de3e46
Alvin Heng <alvin.k.heng@gmail.com>
2012-04-09 20:01:01 -0700

    first commit

---
+++ Movie-Maker/Movie-MakerTests/Movie_MakerTests.m
@@ -0,0 +1,32 @@
+//
+//  Movie_MakerTests.m
+//  Movie-MakerTests
+//
+//  Created by localaheng on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Movie_MakerTests.h"
+
+@implementation Movie_MakerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Movie-MakerTests");
+}
+
+@end
