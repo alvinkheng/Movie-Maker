ffa493686ec532fe1216b555bd24b0bd3aa70caf
Alvin Heng <alvin.k.heng@gmail.com>
2012-05-30 18:51:13 -0700

    removing files

--- Projects/Movie-Maker-CPP/Movie-Maker-CPP/Movie-Maker-CPP/main.frag.glsl
+++
@@ -1,5 +0,0 @@
-uniform sampler2D textureMap;
-
-void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-}
--- Projects/Movie-Maker-CPP/Movie-Maker-CPP/Movie-Maker-CPP/main.vert.glsl
+++
@@ -1,7 +0,0 @@
-attribute vec3 positionIn;
-attribute vec2 texcoordIn;
-
-void main() {
-    gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * vec4(positionIn, 1);
-    
-}
