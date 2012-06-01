8f8972300041a117a8d654cb5a81ad3aaec8f5a2
Alvin Heng <alvin.k.heng@gmail.com>
2012-06-01 03:45:29 -0700

    pushing ntk

--- Projects/multikinect/RGBDemo.build/Debug/rgbd-multikinect.build/Script-434ABB1EBF7D4317A57D66B7.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/localaheng/Downloads/RGBDemo-Source/rgbdemo/build/multikinect -f /Users/localaheng/Downloads/RGBDemo-Source/rgbdemo/build/multikinect/CMakeScripts/rgbd-multikinect_cmakeRulesBuildPhase.make$CONFIGURATION all
